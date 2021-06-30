c26ec7d0080a933630d864567639033b71b384e3
caiocfv <caio2011dhd@gmail.com>
2021-06-30 00:06:49 -0300

    rota para delete

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ routes.post('/movie/type', TypeController.store);
 routes.get('/movie/type', TypeController.index);
 routes.get('/movie/type/:id', TypeController.show);
 routes.put('/movie/type/:id', TypeController.update);
-// routes.delete('/movie/type', TypeController.destroy);
+routes.delete('/movie/type/:id', TypeController.destroy);
 
 //ROTAS PARA ANOLETIVO
 const MovieController = require('./controllers/MovieController');
